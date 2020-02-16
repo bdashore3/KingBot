b96477bbaeae8c1f1fb00bab0c8cec2578feca7f
kingbri <bdashore3@gmail.com>
2020-02-15 22:03:34 -0500

    king: add stuff to be executed by webhook
    Make sure the bot says that it's executing commands.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- king.js
+++ king.js
@@ -50,10 +50,14 @@ const subscription = async (listener) => {
 	return await listener.subscribeToStreamChanges(userId, async (stream) => {
 		if (stream) {
 			console.log(`${stream.userDisplayName} just went live with title: ${stream.title}`);
+			client.action(info.channel, `Executing startup commands...`);
+			chatHelper.timer(info.channel, "start", "follow", "600000");
 		} else {
 			// no stream, no display name
 			const user = await apiClient.helix.users.getUserById(userId);
 			console.log(`${user.displayName} just went offline`);
+			client.action(info.channel, `Executing stop commands...`)
+			chatHelper.timer(info.channel, "stop", "follow");
 		}
 	});
 }
