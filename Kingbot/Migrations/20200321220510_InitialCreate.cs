062bf5b0a11dcd1fa2bb871b9105225b617e8c73
kingbri <bdashore3@gmail.com>
2020-04-25 10:32:04 -0400

    Add EF Core migration files
    Signed-off-by: kingbri <bdashore3@gmail.com>

---
+++ Kingbot/Migrations/20200321220510_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kingbot.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Index = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Intervals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Index = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Intervals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Index = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+
+            migrationBuilder.DropTable(
+                name: "Intervals");
+
+            migrationBuilder.DropTable(
+                name: "Quotes");
+        }
+    }
+}
---
+++ Kingbot/Migrations/20200321230016_AddLinq.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kingbot.Migrations
+{
+    public partial class AddLinq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
